20628730651e3881fcc00e7f6976d37111f1cd00
LiamGunning07 <liamgunning07@gmail.com>
2024-01-25 15:20:34 -0700

    Add file for character count event listener

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  // Use an appropriate selector for the textarea element inside the .new-tweet section
+  $(".new-tweet textarea").on("keypress", function () {
+    // Your event handling code goes here
+    let characterCount = $(this).val().length;
+    let remainingCharacters = 140 - characterCount;
+
+    // Update the character count in the output element
+    $(".counter").text(remainingCharacters);
+
+    if ( remainingCharacters < 0) { // Logic for counter color when user exceeds character limit
+      $(".counter").addClass("over-limit");
+    } else {
+      $(".counter").removeClass("over-limit");
+    }
+  });
+});
