087db2cf544d3dfdc140f1fa173c64d9c0c8a8df
LiamGunning07 <liamgunning07@gmail.com>
2024-01-26 17:14:20 -0700

    Add dynamic tweet rendering

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,20 +4,34 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
-
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 const createTweetElement = function(tweetData) {
-  const $tweet = $(`<article>
+  const $tweet = $(`
+  <br>
+  <article>
   <h5><img src= "${tweetData.user.avatars}"> ${tweetData.user.name}<span>${tweetData.user.handle}</span></h5>     <p>${tweetData.content.text}</p>
   <footer>
   <time>${tweetData.user.created_at}</time>
@@ -28,11 +42,19 @@ const createTweetElement = function(tweetData) {
     </div>
   </footer> 
   </article>`);
+
   return $tweet;
 }
 
-const $tweet = createTweetElement(tweetData);
+const renderTweets = function(tweets) {
+tweets.forEach(tweet => {
+  //createTweetElement(tweet);
+  $('.tweets-container').append(createTweetElement(tweet));
+});
+};
 
+/*const $tweet = createTweetElement(tweetData);
 console.log($tweet);
-console.log($('.tweets-container'));
-$('.tweets-container').append($tweet);
+$('.tweets-container').append($tweet);*/
+
+renderTweets(data);
